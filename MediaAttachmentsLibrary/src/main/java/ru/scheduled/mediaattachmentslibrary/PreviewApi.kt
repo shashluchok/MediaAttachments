76a96639b2ee338eb2641f85d6be7ea3398b01e8
Armen Sarkisyan <palanka9@mail.ru>
2022-06-21 16:18:16 +0300

    Попытка фикса апи интерфейса

--- MediaAttachmentsLibrary/src/main/java/ru/scheduled/mediaattachmentslibrary/PreviewApi.kt
+++ MediaAttachmentsLibrary/src/main/java/ru/scheduled/mediaattachmentslibrary/PreviewApi.kt
@@ -3,12 +3,15 @@ package ru.scheduled.mediaattachmentslibrary
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
-interface PreviewApi {
+interface PreviewApi  {
+    @GET("{Path}")
     abstract fun loadPreview(
         @Query("bucket") bucket: String = "media",
         @Query("key") key: String,
         @Query("resize") resize: String? = null,
+        @Path("Path") path:String
     ): Call<ResponseBody>
 }
